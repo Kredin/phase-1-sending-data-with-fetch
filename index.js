4a5a110f4f14ff96c0247a4f1568b1fea4c2eb07
Blade <bladeprojectz@gmail.com>
2024-03-01 12:45:18 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+  const idz = document.querySelector("body");
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      let p = document.createElement("p");
+      p.textContent = data.id;
+      idz.appendChild(p);
+    })
+    .catch((error) => {
+      let p = document.createElement("p");
+      p.textContent = error;
+      idz.appendChild(p);
+    });
+}
+
+submitData("B", "M");
